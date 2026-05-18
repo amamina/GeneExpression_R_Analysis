ff77eec8abd69ea738f9a8758b2b92277ac6f902
amina_bioinfo <amninaayub@gmail.com>
2026-05-18 14:19:10 +0500

    Update 01_data_loading.R

--- scripts/01_data_loading.R
+++ scripts/01_data_loading.R
@@ -1,1 +1,5 @@
-# Data loading script
+# load series and platform data from GEO
+gset <- getGEO("GSE10500", GSEMatrix =TRUE, AnnotGPL=TRUE)
+if (length(gset) > 1) idx <- grep("GPL8300", attr(gset, "names")) else idx <- 1 
+gset <- gset[[idx]]
+
