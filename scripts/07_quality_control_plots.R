961f5e1779c3e59affe22b13f441c1f3d19fb02f
amina_bioinfo <amninaayub@gmail.com>
2026-05-19 12:20:50 +0500

    Create 07_quality_control_plots.R

---
+++ scripts/07_quality_control_plots.R
@@ -0,0 +1,15 @@
+# Quality control: distribution of adjusted p-values
+
+tT <- topTable(fit2, adjust = "fdr", sort.by = "B", number = Inf)
+
+# Plot histogram
+png("results/plots/pvalue_histogram.png", width = 800, height = 600)
+
+hist(
+  tT$adj.P.Val,
+  col = "grey",
+  border = "white",
+  xlab = "Adjusted P-value",
+  ylab = "Number of genes",
+  main = "Distribution of Adjusted P-values"
+)
