60385473c89295107e1d2e077629a5adb373e7ee
amina_bioinfo <amninaayub@gmail.com>
2026-05-19 11:41:14 +0500

    Create 06_differential_expression_results.R

---
+++ scripts/06_differential_expression_results.R
@@ -0,0 +1,27 @@
+# Apply empirical Bayes moderation
+fit2 <- eBayes(fit2, 0.01)
+
+# Extract top differentially expressed genes
+tT <- topTable(
+  fit2,
+  adjust = "fdr",
+  sort.by = "B",
+  number = 250
+)
+
+# Select relevant annotation and statistics columns
+tT <- subset(
+  tT,
+  select = c(
+    "ID",
+    "adj.P.Val",
+    "P.Value",
+    "t",
+    "B",
+    "logFC",
+    "GenBank.Accession",
+    "Platform_SPOTID",
+    "Gene.symbol",
+    "Gene.title"
+  )
+)
