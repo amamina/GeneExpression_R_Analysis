1fdc67661ef128278104513a0f1ed03610c730e0
amina_bioinfo <amninaayub@gmail.com>
2026-05-19 11:09:29 +0500

    Update 01_load_GEO_data.R

--- scripts/01_load_GEO_data.R
+++ scripts/01_load_GEO_data.R
@@ -1,7 +1,29 @@
-# load series and platform data from GEO
-gset <- getGEO("GSE10500", GSEMatrix =TRUE, AnnotGPL=TRUE)
-if (length(gset) > 1) idx <- grep("GPL8300", attr(gset, "names")) else idx <- 1 
+# Load required libraries
+library(Biobase)
+library(GEOquery)
+library(limma)
+library(umap)
+library(maptools)
+library(DESeq2)
+library(edgeR)
+library(ggplot2)
+
+# Show session information
+sessionInfo()
+
+# Load series and platform data from GEO
+gset <- getGEO(
+  "GSE10500",
+  GSEMatrix = TRUE,
+  AnnotGPL = TRUE
+)
+
+# Select GPL8300 platform
+if (length(gset) > 1) {
+  idx <- grep("GPL8300", attr(gset, "names"))
+} else {
+  idx <- 1
+}
+
 gset <- gset[[idx]]
-# After selection, gset contains the GEO ExpressionSet
-# corresponding to platform GPL8300
 
